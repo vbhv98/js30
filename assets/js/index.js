b8fa3c0489bb4a98c9e657ebb2283ae590797bea
vaibhav bhawalkar <vaibhavbhawalkar98.vb@gmail.com>
2019-06-10 15:26:12 +0530

    Disabled projects

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,8 @@
 const projectList = document.getElementById('project-list')
-const cardUI = (number, name, onClickID) => `
-  <article class="project-card" id="${onClickID}">
+const cardUI = (number, name, onClickID, enabled) => `
+  <article class="project-card ${
+    enabled ? 'done' : 'not-done'
+  }" id="${enabled && onClickID}">
     <span class="project-number">#${number}</span>
     <h3 class="project-name">${name}</h3>
   </article>
@@ -11,7 +13,7 @@ let projectIDs = []
 
 projects.forEach(project => {
   const ID = `${project.name}_${project.number}`
-  projectListHTML += cardUI(project.number, project.name, ID)
+  projectListHTML += cardUI(project.number, project.name, ID, project.enabled)
   projectIDs.push(ID)
 })
 
@@ -19,7 +21,7 @@ projectList.innerHTML = projectListHTML
 
 projectIDs.forEach(ID =>
   document.getElementById(ID).addEventListener('click', () => {
-    const name = ID.split('_')[0]
+    const name = ID.split('_')[0].replace(' ', '-')
     window.open(`/js30/${name}`, '_blank')
   }),
 )
--- projects.js
+++ projects.js
@@ -1,9 +1,43 @@
-const names = ['drum-kit']
+const names = [
+  'drum-kit',
+  'Clock',
+  'CSS-Variables',
+  'Array-Cardio 1',
+  'Image-Gallery',
+  'Type-Ahead',
+  'Array-Cardio 2',
+  'HTML5-Canvas',
+  'Check-Multiple-Checkboxes',
+  'Custom-HTML5-Video-Player',
+  'Key-Sequence-Detection',
+  'Slide-In-on-Scroll',
+  'Object-and-Arrays-Reference-VS-Copy',
+  'LocalStorage-and-Event-Delegation',
+  'CSS-Text Shadow-Mouse-Move-Effect',
+  'Sorting-Band-Names-without-articles',
+  'Tally-String-Times-with-Reduce',
+  'Unreal-Webcam-Fun',
+  'Native-Speech-Recognition',
+  'Geolocation-based-Speedometer-and-Compass',
+  'Follow-Along-Links',
+  'Speech-Synthesis',
+  'Sticky-Nav',
+  'Event-Capture-Propagation-Bubbling-and-Once',
+  'Stripe-Follow-Along-Dropdown',
+  'Click-and Drag-to-Scroll',
+  'Video-Speed-Controller-UI',
+  'Countdown-Clock',
+  'Whack-A-Mole-Game',
+  "That's-All-Folks!",
+]
+
+const doneTill = 1
 
 const projects = names.map((name, i) => {
   return {
-    name,
+    name: name.split('-').join(' '),
     number: i + 1,
+    enabled: doneTill >= i + 1,
   }
 })
 
