b22fc308103ce14a4dc776ab4ed2533c9f8a58be
vaibhav bhawalkar <vaibhavbhawalkar98.vb@gmail.com>
2019-06-10 16:54:14 +0530

    responsive

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,17 @@
 const projectList = document.getElementById('project-list')
+
+text_truncate = (str, length = 100, ending = '...') => {
+  if (str.length > length)
+    return str.substring(0, length - ending.length) + ending
+  return str
+}
+
 const cardUI = (number, name, onClickID, enabled) => `
   <article class="project-card ${
     enabled ? 'done' : 'not-done'
   }" id="${enabled && onClickID}">
-    <span class="project-number">#${number}</span>
-    <h3 class="project-name">${name}</h3>
+    <span class="project-number">${String(number).padStart(2, '0')}</span>
+    <h3 class="project-name">${text_truncate(name, 24)}</h3>
   </article>
 `
 
@@ -14,7 +21,7 @@ let projectIDs = []
 projects.forEach(project => {
   const ID = `${project.name}_${project.number}`
   projectListHTML += cardUI(project.number, project.name, ID, project.enabled)
-  projectIDs.push(ID)
+  project.enabled && projectIDs.push(ID)
 })
 
 projectList.innerHTML = projectListHTML
